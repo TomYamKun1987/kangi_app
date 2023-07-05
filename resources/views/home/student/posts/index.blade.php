0bad661c9103cbc04d9fbb516db78f1368baac92
Hiromi Tanaka <a10hecho.pecho.ht@gmail.com>
2023-07-06 00:22:12 +0900

    ビューの作成

---
+++ resources/views/home/student/posts/index.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang='ja'>
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>生徒情報投稿一覧</title>
+</head>
+
+<body>
+  <header>
+    <nav>
+      <div>
+        <a href="{{ route('posts.index') }}">ホーム</a>
+      <div>
+      <div>
+        <a href="#">戻る</a>
+      </div>
+
+    </nav>
+  </header>
+  
+  <main>
+    <article>
+      <div>
+        <h1>引き継ぎ記録一覧</h1>
+      </div>
+    </article>
+  </main>
+
+  <footer>
+
+  </footer>
+
+</body>
+
+</html>
