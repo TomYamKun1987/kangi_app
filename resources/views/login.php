0085007c3563c00957bb17719fd64af8aed32797
Hiromi Tanaka <a10hecho.pecho.ht@gmail.com>
2023-07-07 19:46:46 +0900

    ログイン画面更新しました。

---
+++ resources/views/login.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang='ja'>
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>ログイン画面</title>
+  <h1>咸宜寮管理アプリ</h1>
+</head>
+
+<body>
+  <header>
+       <h2>ログインページ</h2>
+  </header>
+  
+  <main>
+    <form action="" method="POST">
+    <article>
+      <table>
+        <tr>
+          <td>職員ID</td>
+          <td>
+             <input type="text" name="user_id">
+          </td>
+        </tr>
+        <tr>
+          <td>パスワード</td>
+          <td>
+            <input type="text" name="password">
+          </td>
+        </tr>
+      </table>
+      <input type="submit" value="ログイン">
+
+      <div>
+        <a href="#">新規登録はこちら</a>
+      <div>
+
+    </article>
+   </form>
+  </main>
+
+  <footer>
+
+  </footer>
+
+</body>
+
+</html>
