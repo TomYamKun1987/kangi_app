1db9f101e4a9333b6d76fe6413de04df0f0872d0
Hiromi Tanaka <a10hecho.pecho.ht@gmail.com>
2023-07-10 00:06:21 +0900

    マイグレーションファイル作成

---
+++ database/migrations/create_staffs_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('staffs', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('staff_code');
+            $table->string('last_name');
+            $table->string('first_name');
+            $table->string('password');
+            $table->timestamps('updated_at');
+            $table->timestamps('created_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('staffs');
+    }
+};
---
+++ database/migrations/create_students_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('last_name');
+            $table->string('first_name');
+            $table->dateTime('birthday');
+            $table->integer('grade');
+            $table->timestamps('updated_at');
+            $table->timestamps('created_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+};
